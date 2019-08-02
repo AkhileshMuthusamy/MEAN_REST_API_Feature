27a4d62f5c15a900e5f8c7c72b80969b1d87cfd9
Akhilesh Muthusamy <AkhileshM@outlook.in>
2019-08-02 18:58:50 +0530

    Added logic to upload single image

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const app = require('express')();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const multer = require('multer');
 
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+let storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, './uploads/Images');
+	},
+	filename: (req, file, cb) => {
+		console.log(`${file.fieldname}_${Date.now()}_${file.originalname}`);
+		cb(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
+	}
+});
+const upload = multer({ storage: storage });
+
+app.get('/', (req, res) => {
+	res.status(200).send('Welcome to MEAN stack RESTful API!');
+});
+
+app.post('/api/upload', upload.single('avatar'), (req, res) => {
+	return res.status(200).end('File uploaded successfuly!');
+});
 
 app.listen(port, () => {
 	console.log(`Server is running on port: ${port}`);
